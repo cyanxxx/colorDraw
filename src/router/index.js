fb8b47c19efefb6fd559f336a1b0a078cdd11d4c
cyanxxx <504512494@qq.com>
2018-11-13 15:42:16 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Room from '../views/Room.vue'
+import OfflineRoom from '../views/OfflineRoom.vue'
 import Main from '../views/Main.vue'
 import store from '../store'
 import {GAMING} from '../utils/constant'
 
 Vue.use(Router)
-
+var test = true
 export default new Router({
   mode: 'history',
   routes: [
@@ -19,13 +20,18 @@ export default new Router({
       name:"room",
       component:Room,
       //开始游戏
-      beforeEnter(){
-        if(store.getters.status === 'GAMING'){
+      beforeEnter: (to, from, next)=>{
+        if(store.getters.status === 'GAMING' || test){
           next()
         }else{
           next({path:"/"})
         }
       }
+    },
+    {
+      path:'/offlineRoom:id',
+      name:"offlineRoom",
+      component:OfflineRoom,
     }
   ]
 })
