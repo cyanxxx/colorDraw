003501061e4cc796c1e7de00e10a484fdb5fafcd
cyanxxx <504512494@qq.com>
2018-11-21 09:49:31 +0800

    merge

--- src/permission.js
+++ src/permission.js
@@ -5,6 +5,7 @@ import LS from './utils/LocalStorage'
 import webSocketSerive from './webSocketSerive'
 import bar from './bar'
 import tip from './tip'
+
 router.beforeEach((to, from, next) => {
   bar.on()
   tip.msg('加载中...')
